83b88956f8bb3f80d52b37cf5b8dab6ed5f3c4c7
KC07 <cyrille.kirscher@etudiant.univ-lille1.fr>
2016-03-23 16:39:17 +0100

    update fields

--- REST/src/main/java/fr/univ_lille/iut/User.java
+++ REST/src/main/java/fr/univ_lille/iut/User.java
@@ -6,6 +6,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class User {
     private static int nbUser;
     private int id;
+    
+    private String nom;
+    private String prenom;
     private String login;
     private String mdp;
     private String adresse;
@@ -13,9 +16,12 @@ public class User {
     private String ville;
     private String mail;
     private int age;
+    private String mobile;
 
-    public User(String login, String mdp, String adresse, int codePostal, String ville, String mail, int age) {
+    public User(String nom, String prenom, String login, String mdp, String adresse, int codePostal, String ville, String mail, int age, String mobile) {
     	this();
+    	this.nom = nom;
+    	this.prenom = prenom;
         this.login = login;
         this.mdp = mdp;
         this.adresse = adresse;
@@ -23,20 +29,24 @@ public class User {
         this.ville = ville;
         this.mail = mail;
         this.age = age;
+        this.mobile = mobile;
     }
 
     public User() {
     	this.id = ++nbUser;
     }
+    
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
 
-    public String getLogin() {
-        return login;
-    }
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
+    public String getMdp() { return mdp; }
+    
     public String getMail() {
         return mail;
     }
@@ -57,9 +67,6 @@ public class User {
         return id;
     }
 
-    public String getMdp() {
-        return mdp;
-    }
 
     public String getVille() {
         return ville;
@@ -92,6 +99,9 @@ public class User {
     public void setMail(String mail) {
         this.mail = mail;
     }
+    
+    public String getMobile() { return mobile; }
+    public void setMobile(String mobile) { this.mobile = mobile; }
 
     public boolean equals(Object u) {
         return login.equals(((User) u).login) || mail.equals(((User) u).mail);
