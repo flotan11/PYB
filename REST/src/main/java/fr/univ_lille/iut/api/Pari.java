69f8d9bc446121cda0c297820fffb1bcb76c5ab8
KC07 <cyrille.kirscher@etudiant.univ-lille1.fr>
2016-03-29 16:28:47 +0200

    refactor

--- REST/src/main/java/fr/univ_lille/iut/api/Pari.java
+++ REST/src/main/java/fr/univ_lille/iut/api/Pari.java
@@ -4,61 +4,56 @@
  */
 package fr.univ_lille.iut.api;
 
-/**
- *
- * @author youdelice
- */
 public class Pari {
     private static int idInc;
 
     private int id;
     private String mdp;
     private String nom;
-    private String side1;
-    private String side2;
-    private int miseSide1;
-    private int miseSide2;
+    private String sideOne;
+    private String sideTwo;
+    private int miseSideOne;
+    private int miseSideTwo;
     private String description;
-    private int coteSide1;
-    private int coteSide2;
+    private int coteSideOne;
+    private int coteSideTwo;
     private int miseMin;
     private int total;
-    private boolean prive;
+    private boolean privacy;
     
     public Pari() {
-    	this.id = idInc++;
+    	this.id = ++idInc;
+    }
+    
+    public Pari(String nom, String sideOne, String sideTwo, String description, int miseMin, boolean privacy) {
+    	this();
+    	this.nom = nom;
+    	this.sideOne = sideOne;
+    	this.sideTwo = sideTwo;
+    	this.description = description;
+    	this.miseMin = miseMin;
+    	this.privacy = privacy;
     }
 
-    public Pari(String mdp, String nom, String side1, String side2, int miseSide1, int miseSide2, String description, int coteSide1, int codeSide2, int miseMin, int total, boolean priver) {
-        this();
-        this.mdp = mdp;
-        this.nom = nom;
-        this.side1 = side1;
-        this.side2 = side2;
-        this.miseSide1 = miseSide1;
-        this.miseSide2 = miseSide2;
-        this.description = description;
-        this.coteSide1 = coteSide1;
-        this.coteSide2 = codeSide2;
-        this.miseMin = miseMin;
-        this.total = total;
-        this.prive = priver;
+    public Pari(String mdp, String nom, String sideOne, String sideTwo, String description, int miseMin, boolean privacy) {
+        this(nom, sideOne, sideTwo, description, miseMin, privacy);
+    	this.mdp = mdp;
     }
 
-    public void setCoteSide1(int coteSide1) {
-        this.coteSide1 = coteSide1;
+    public void setCoteSideOne(int coteSideOne) {
+        this.coteSideOne = coteSideOne;
     }
 
-    public void setCoteSide2(int coteSide2) {
-        this.coteSide2 = coteSide2;
+    public void setCoteSideTwo(int coteSideTwo) {
+        this.coteSideTwo = coteSideTwo;
     }
 
-    public int getCoteSide1() {
-        return coteSide1;
+    public int getCoteSideOne() {
+        return coteSideOne;
     }
 
-    public int getCoteSide2() {
-        return coteSide2;
+    public int getCoteSideTwo() {
+        return coteSideTwo;
     }
 
     public String getDescription() {
@@ -73,10 +68,6 @@ public class Pari {
         return mdp;
     }
 
-    public static void setIdInc(int idInc) {
-        Pari.idInc = idInc;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -89,20 +80,20 @@ public class Pari {
         this.nom = nom;
     }
 
-    public void setSide1(String side1) {
-        this.side1 = side1;
+    public void setSideOne(String sideOne) {
+        this.sideOne = sideOne;
     }
 
-    public void setSide2(String side2) {
-        this.side2 = side2;
+    public void setSideTwo(String sideTwo) {
+        this.sideTwo = sideTwo;
     }
 
-    public void setMiseSide1(int miseSide1) {
-        this.miseSide1 = miseSide1;
+    public void setMiseSideOne(int miseSideOne) {
+        this.miseSideOne = miseSideOne;
     }
 
-    public void setMiseSide2(int miseSide2) {
-        this.miseSide2 = miseSide2;
+    public void setMiseSideTwo(int miseSideTwo) {
+        this.miseSideTwo = miseSideTwo;
     }
 
     public void setDescription(String description) {
@@ -117,52 +108,52 @@ public class Pari {
         this.total = total;
     }
 
-    public void setPrive(boolean priver) {
-        this.prive = priver;
+    public void setPrivacy(boolean privacy) {
+        this.privacy = privacy;
     }
 
     public String getNom() {
         return nom;
     }
 
-    public String getSide1() {
-        return side1;
+    public String getSideOne() {
+        return sideOne;
     }
 
-    public String getSide2() {
-        return side2;
+    public String getSideTwo() {
+        return sideTwo;
     }
 
     public int getTotal() {
         return total;
     }
 
-    public boolean isPrive() {
-        return prive;
+    public boolean getPrivacy() {
+        return privacy;
     }
 
     public int getMiseMin() {
         return miseMin;
     }
 
-    public int getMiseSide1() {
-        return miseSide1;
+    public int getMiseSideOne() {
+        return miseSideOne;
     }
 
-    public int getMiseSide2() {
-        return miseSide2;
+    public int getMiseSideTwo() {
+        return miseSideTwo;
     }
     
-    public void addMiseSide1(int x) {
-        miseSide1 += x;
+    public void addMiseSideOne(int mise) {
+        this.miseSideOne += mise;
+        setTotal(this.total + mise);
+        setCoteSideOne(this.total / this.miseSideOne);
     }
     
-    public void addMiseSide2(int x) {
-        miseSide2 += x;
+    public void addMiseSideTwo(int mise) {
+        this.miseSideTwo += mise;
+        setTotal(this.total + mise);
+        setCoteSideTwo(this.total / this.miseSideTwo);
     }
-    
-    
-    
-    
     
 }
